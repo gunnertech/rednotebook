d9439f986cdc6f460ece345a9bf92e2ec4d17e54
Cody Swann <cody@gunnertech.com>
2016-12-24 16:17:37 -0500

    minor

--- app/routes.js
+++ app/routes.js
@@ -44,6 +44,9 @@ export default (app, router, passport) => {
   // ### Express Middlware to use for all requests
   router.use((req, res, next) => {
 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, EncryptionKey");
+
     console.log('I sense a disturbance in the force...'); // DEBUG
     // Make sure we go to the next routes and don't stop here...
     next();
